fcd34da77a06349fa7bbb2d9c41c18afea35ddb5
Gershaveut <vova1234567890012011@yandex.ru>
2024-05-29 14:29:51 +0300

    Working on Menu, Chat

--- app/src/main/java/com/gershaveut/service/chatOFG/ui/COFragment.kt
+++ app/src/main/java/com/gershaveut/service/chatOFG/ui/COFragment.kt
@@ -15,26 +15,36 @@ import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Send
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.gershaveut.coapikt.Message
@@ -120,7 +130,37 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 		}
 	}
 	
-	@OptIn(ExperimentalLayoutApi::class)
+	@OptIn(ExperimentalMaterial3Api::class)
+	@Composable
+	fun ServiceAlertDialog(title: String, onDismissRequest: () -> Unit, onConfirmRequest: () -> Unit, content: @Composable () -> Unit, dismissText: String? = null, confirmText: String? = null) {
+		BasicAlertDialog(onDismissRequest) {
+			Surface( shape = RoundedCornerShape(15.dp) ) {
+				Column {
+					Text(title, fontSize = 20.sp, modifier = Modifier.padding(start = 24.dp, end = 24.dp, bottom = 16.dp, top = 24.dp))
+					Column( modifier = Modifier.padding(horizontal = 24.dp) ) {
+						content()
+					}
+					Row(modifier = Modifier.padding(24.dp).align(Alignment.End)) {
+						TextButton(
+							{
+							
+							},
+							modifier = Modifier.padding(end = 10.dp)
+						) {
+							Text(dismissText ?: stringResource(R.string.dialog_cancel))
+						}
+						TextButton({
+							onConfirmRequest()
+						}
+						) {
+							Text(confirmText ?: stringResource(R.string.dialog_confirm))
+						}
+					}
+				}
+			}
+		}
+	}
+	
 	@Preview(device = "spec:width=411dp,height=891dp")
 	@Composable
 	fun Menu() {
@@ -138,14 +178,47 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 					textAlign = TextAlign.Center,
 					modifier = Modifier.padding(bottom = 20.dp)
 				)
-				Button(
-					{
-						//coContent.coContent.closeDrawers()
-						
+				val openLoginDialog = remember { mutableStateOf(false) }
+				
+				Button( {
+						openLoginDialog.value = true
 						LoginDialogFragment().show(parentFragmentManager, null)
 					}, modifier = Modifier.padding(bottom = 16.dp)
 				) {
 					Text(stringResource(R.string.co_connect), fontSize = 18.sp)
+				}
+				
+				if (openLoginDialog.value) {
+					ServiceAlertDialog(
+						stringResource(R.string.login_login),
+						confirmText = stringResource(R.string.login_login),
+						onDismissRequest = {},
+						onConfirmRequest = {},
+						content = {
+							val hostname = remember{mutableStateOf("")}
+							val port = remember{mutableStateOf("")}
+							val name = remember{mutableStateOf("")}
+							
+							TextField(
+								hostname.value, { text ->
+									hostname.value = text
+								},
+								placeholder = { Text(stringResource(R.string.login_hostname)) }
+							)
+							TextField(
+								port.value, { text ->
+									port.value = text
+								},
+								placeholder = { Text(stringResource(R.string.login_port)) }
+							)
+							TextField(
+								name.value, { text ->
+									name.value = text
+								},
+								placeholder = { Text(stringResource(R.string.login_name)) }
+							)
+						}
+					)
 				}
 				
 				HorizontalDivider()
@@ -157,18 +230,17 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 					modifier = Modifier.padding(top = 16.dp)
 				) {
 					items(connections) { connection ->
-						FlowColumn(
-							modifier = Modifier
-								.fillMaxWidth()
-								.height(35.dp)
+						Box(
+							modifier = Modifier.size(height = 35.dp, width = 350.dp)
 						) {
-							Text(connection.userName ?: "null", modifier = Modifier.weight(1f))
-							Text(
-								"${connection.hostname}:${connection.port}",
-								fontSize = 12.sp,
-								color = Color.Gray,
-								modifier = Modifier.weight(1f)
-							)
+							Column(modifier = Modifier.align(Alignment.CenterStart)) {
+								Text(connection.userName ?: "null")
+								Text(
+									"${connection.hostname}:${connection.port}",
+									fontSize = 12.sp,
+									color = Color.Gray
+								)
+							}
 							
 							IconButton(
 								{
@@ -212,7 +284,9 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 									
 									popupMenu.show()
 								},
-								modifier = Modifier.size(30.dp),
+								modifier = Modifier
+									.size(30.dp)
+									.align(Alignment.CenterEnd)
 							) {
 								Icon(Icons.Outlined.MoreVert, null)
 							}
@@ -223,7 +297,6 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 		}
 	}
 	
-	@OptIn(ExperimentalLayoutApi::class)
 	@Preview(device = "spec:width=411dp,height=891dp")
 	@Composable
 	fun Chat() {
@@ -233,17 +306,23 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 					ModalDrawerSheet(modifier = Modifier.width(250.dp)) {
 						Column(horizontalAlignment = Alignment.CenterHorizontally) {
 							Button(
-								{}, modifier = Modifier.fillMaxWidth()
+								{}, modifier = Modifier
+									.fillMaxWidth()
+									.padding(horizontal = 5.dp)
 							) {
 								Text(stringResource(R.string.co_disconnect))
 							}
 							Button(
-								{}, modifier = Modifier.fillMaxWidth()
+								{}, modifier = Modifier
+									.fillMaxWidth()
+									.padding(horizontal = 5.dp)
 							) {
 								Text(stringResource(R.string.co_broadcast_send))
 							}
 							Button(
-								{}, modifier = Modifier.fillMaxWidth()
+								{}, modifier = Modifier
+									.fillMaxWidth()
+									.padding(horizontal = 5.dp)
 							) {
 								Text(stringResource(R.string.co_custom_message))
 							}
@@ -252,12 +331,16 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 							
 							LazyColumn {
 								items(users) { user ->
-									FlowRow {
-										Text(user, modifier = Modifier.weight(1f))
+									Box( modifier = Modifier
+										.padding(horizontal = 5.dp)
+										.fillMaxWidth() ) {
+										Text(user, modifier = Modifier.align(Alignment.CenterStart))
 										IconButton( {
 											
 											},
-											modifier = Modifier.size(30.dp)
+											modifier = Modifier
+												.size(30.dp)
+												.align(Alignment.CenterEnd)
 										) {
 											Icon(Icons.Outlined.MoreVert, null)
 										}
@@ -267,23 +350,121 @@ class COFragment : Fragment(), COClient.Listener, ServiceConnection {
 						}
 					}
 				}) {
-				FlowRow {
-					Text(
-						"", modifier = Modifier
-							.fillMaxWidth()
-							.height(700.dp)
-					)
-					TextField(stringResource(R.string.co_message), {})
-					IconButton( {
-					
+				//Chat content
+				
+				Column {
+					LazyColumn( modifier = Modifier.weight(15f) ) {
+						items(listOf(
+							ChatMessage("Ты поедешь в Пирогово, будешь меня бранить Сереже?", false),
+							ChatMessage("Я ни с кем не говорил, ни с Таней, дочерью.", true),
+							ChatMessage("Но с Таней, сестрой, говорил?", false),
+							ChatMessage("Да.", true),
+							ChatMessage("Что же она говорила?", false),
+							ChatMessage("То же, что тебе… мне тебя защищала, тебе, вероятно, за меня говорила.", true),
+							ChatMessage("Да, она ужасно строга была ко мне. Слишком строга. Я не заслуживаю.", false),
+							ChatMessage("Пожалуйста, не будем говорить, уляжется, успокоится и, бог даст, уничтожится.", true),
+							ChatMessage("Не могу я не говорить. Мне слишком тяжело жить под вечным страхом. Теперь, если он заедет, начнется опять. Он не говорил ничего, но, может быть, заедет.", false),
+							ChatMessage("Только что надеялся успокоиться, как опять ты будто приготавливаешь меня к неприятному ожиданию.", true),
+							ChatMessage("Что же мне делать? Это может быть, он сказал Тане. Я не звала. Может быть, он заедет.", false),
+							ChatMessage("Заедет он или не заедет, неважно, даже твоя поездка не важна, важно, как я говорил тебе, два года назад говорил тебе, твое отношение к твоему чувству. Если бы ты признавала свое чувство нехорошим, ты бы не стала даже и вспоминать о том, заедет ли он, и говорить о нем.", true),
+							ChatMessage("Ну, как же быть мне теперь?", false),
+							ChatMessage("Покаяться в душе в своем чувстве.", true),
+							ChatMessage("Не умею каяться и не понимаю, что это значит.", false),
+							ChatMessage("Это значит обсудить самой с собой, хорошо ли то чувство, которое ты испытываешь к этому человеку, или дурное.", true),
+							ChatMessage("Я никакого чувства не испытываю, ни хорошего, ни дурного.", false),
+							ChatMessage("Это неправда.", true),
+							ChatMessage("Чувство это так неважно, ничтожно.", false),
+							ChatMessage("Все чувства, а потому и самое ничтожное, всегда или хорошие, или дурные в наших глазах, и потому и тебе надо решить, хорошее ли это было чувство, или дурное.", true),
+							ChatMessage("Нечего решать, это чувство такое неважное, что оно не может быть дурным. Да и нет в нем ничего дурного.", false),
+							ChatMessage("Нет, исключительное чувство старой замужней женщины к постороннему мужчине — дурное чувство.", true),
+							ChatMessage("У меня нет чувства к мужчине, есть чувство к человеку.", false),
+							ChatMessage("Да ведь человек этот мужчина.", true),
+							ChatMessage("Он для меня не мужчина. Нет никакого чувства исключительного, а есть то, что после моего горя мне было утешение музыка, а к человеку нет никакого особенного чувства.", false),
+							ChatMessage("Зачем говорить неправду?", true),
+							ChatMessage("Но хорошо. Это было. Я сделала дурно, что заехала, что огорчила тебя. Но теперь это кончено, я сделаю все, чтобы не огорчать тебя.", false),
+							ChatMessage("Ты не можешь этого сделать потому, что все дело не в том, что ты сделаешь — заедешь, примешь, не примешь, дело все в твоем отношении к твоему чувству. Ты должна решить сама с собой, хорошее ли это, или дурное чувство.", true),
+							ChatMessage("Да нет никакого.", false),
+							ChatMessage("Это неправда. И вот это-то и дурно для тебя, что ты хочешь скрыть это чувство, чтобы удержать его. А до тех пор, пока ты не решишь, хорошее это чувство или дурное, и не признаешь, что оно дурное, ты будешь не в состоянии не делать мне больно. Если ты признаешь, как ты признаешь теперь, что чувство это хорошее, то никогда не будешь в силах не желать удовлетворения этого чувства, то есть видеться, а желая, ты невольно будешь делать то, чтобы видеться. Если ты будешь избегать случаев видеться, то тебе будет тоска, тяжело. Стало быть, все дело в том, чтобы решить, какое это чувство, дурное или хорошее.", true)
+						)) { chatMessage ->
+							@Composable
+							fun messageContent(message: ChatMessage) {
+								Column( horizontalAlignment = Alignment.CenterHorizontally ) {
+									if (message.owner != null) {
+										Text(message.owner!!, color = Color(41, 150, 201), fontSize = 15.sp, modifier = Modifier
+											.padding(top = 10.dp, start = 10.dp, end = 10.dp)
+											.align(Alignment.Start))
+									}
+									
+									if (message.id != null) {
+										Image(ImageBitmap.imageResource(message.id!!), null)
+									}
+									
+									Text(message.text, modifier = Modifier
+										.padding(
+											top = if (message.owner == null) 10.dp else 0.dp,
+											start = 10.dp,
+											bottom = 10.dp,
+											end = 10.dp
+										)
+										.align(Alignment.Start))
+								}
+							}
+							
+							if (chatMessage.isRemote) {
+								Box(
+									modifier = Modifier
+										.padding(top = 5.dp, end = 75.dp, start = 5.dp)
+										.background(
+											color = Color(238, 238, 238),
+											shape = RoundedCornerShape(10.dp)
+										)
+								) {
+									messageContent(chatMessage)
+								}
+							} else {
+								Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+									Box(
+										modifier = Modifier
+											.padding(top = 5.dp, start = 75.dp, end = 5.dp)
+											.background(
+												color = Color(199, 225, 252),
+												shape = RoundedCornerShape(10.dp)
+											)
+									) {
+										messageContent(chatMessage)
+									}
+								}
+							}
+						}
 					}
-					) {
-						Icon(Icons.AutoMirrored.Outlined.Send, null)
+					
+					Row {
+						val message = remember{mutableStateOf("")}
+						
+						TextField(
+							message.value, { text ->
+								message.value = text
+							},
+							modifier = Modifier
+								.weight(1f)
+								.fillMaxWidth(),
+							placeholder = { Text(stringResource(R.string.co_message)) }
+						)
+						IconButton(
+							{
+							
+							},
+							modifier = Modifier.size(50.dp)
+						) {
+							Icon(Icons.AutoMirrored.Outlined.Send, null)
+						}
 					}
 				}
 			}
 		}
 	}
+	
+	class ChatMessage(var text: String, var isRemote: Boolean = true, @DrawableRes var id: Int? = null, var owner: String? = null)
 	
 	fun snackbar(text: String) {
 		Snackbar.make(binding.root, text, 1000).show()
